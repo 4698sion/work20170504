236f02a1681dbfa3e3484c960fcc8fb0f4e714c6
githubuseryu <1254270550@qq.com>
2017-05-04 21:02:16 +0800

    Initial commit

---
+++ src/com/yu/work/Hellow.java
@@ -0,0 +1,10 @@
+package com.yu.work;
+
+/**
+ * Created by YuWenJing on 2017/5/4.
+ */
+public class Hellow {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
